5c335f4d1ae3ed5908b4568704bd61d7d09ba4bb
Tuvshin <tuvshintug@gmail.com>
2024-09-25 16:10:21 +0800

    fuck

--- components/navbar.js
+++ components/navbar.js
@@ -279,6 +279,48 @@ const Navbar = () => {
               >
                 Бидэнтэй холбогдох
               </a>
+              <button
+                onClick={toggleDarkMode}
+                className="relative w-10 h-10 p-2 mr-5 rounded-full bg-gray-200 dark:bg-gray-700 transition-all duration-300 flex items-center justify-center"
+              >
+                <svg
+                  className={`w-6 h-6 absolute transition-opacity duration-150 ${
+                    darkMode ? "opacity-0" : "opacity-100"
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                  />
+                </svg>
+
+                {/* Sun Icon (for light mode) */}
+
+                {/* Moon Icon (for dark mode) */}
+                <svg
+                  className={`w-6 h-6 absolute transition-opacity duration-150 ${
+                    darkMode ? "opacity-100" : "opacity-0"
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 12.79A9 9 0 1111.21 3a7 7 0 109.79 9.79z"
+                  />
+                </svg>
+              </button>
             </div>
           </motion.div>
         )}
