a7e316bcedc0e1accaeaa6fea980a7a8cf5a108a
Tuvshin <tuvshintug@gmail.com>
2024-09-25 12:50:22 +0800

    icon added in the nav button

--- components/navbar.js
+++ components/navbar.js
@@ -82,15 +82,27 @@ const Navbar = () => {
           <div className="hidden md:flex">
             <a
               href="#"
-              className="rounded-2xl border-solid border-2 border-[#F27360] text-white px-4 py-2 "
-              type="button"
-              class="text-white font-medium rounded-lg text-sm px-4 py-2 text-center border-solid bg-transparent border-[#EF5239] border-2"
+              className="flex items-center space-x-2 rounded-xl border-2 text-white font-medium text-sm px-4 py-2 text-center bg-transparent border-[#EF5239]"
               onClick={(e) => {
                 e.preventDefault();
                 scrollToSection("#contact");
               }}
             >
-              Бидэнтэй холбогдох
+              <span>Бидэнтэй холбогдох</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 rotate-[-135deg]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
             </a>
           </div>
 
