437ccadaf9ab2f7ab8dbc9d42ad7f83c0e5bebe1
Tuvshin <tuvshintug@gmail.com>
2024-10-28 12:18:08 +0800

    .

--- app/layout.js
+++ app/layout.js
@@ -2,14 +2,13 @@
 
 import { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import "aos/dist/aos.css"; // Import AOS styles
 import AOS from "aos";
+
 const inter = Inter({ subsets: ["latin"] });
-export const metadata = {
-  title: "CoreTech",
-  description: "Created by coretech",
-};
+
 export default function RootLayout({ children }) {
   useEffect(() => {
     AOS.init({
@@ -17,12 +16,16 @@ export default function RootLayout({ children }) {
       once: true, // Whether animation should happen only once - while scrolling down
     });
   }, []);
+
   const [state, setState] = useState("");
+
   return (
     <html lang="en">
-      <head>
+      <Head>
+        <title>CoreTech</title>
+        <meta name="description" content="Created by coretech" />
         <link rel="icon" href="/tablogo.svg" />
-      </head>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
